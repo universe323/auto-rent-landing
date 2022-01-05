c8823abe0d547ec671be2268c62fcfcf5c35f2a8
Universe323 <universeaso323@gmail.com>
2022-01-05 15:29:17 +0200

    Vrode header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,37 +1,43 @@
-import ImgLogo from "../../icons/logo.svg"
+import ImgLogo from "../../icons/logo.png"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faFacebook, faInstagram, faVk, faWhatsapp} from '@fortawesome/free-brands-svg-icons'
+import AppStore from "../../icons/Social/App Store.svg"
+import PlayMarket from "../../icons/Social/Google play.svg"
 
 const Header = () => {
   return (
     <>
       <header className="header">
-        <div className="container">
+        <div className="header__container">
           <div className="header__logo">
-            <a href="#"><img className="header__logo_img" src={ImgLogo} alt="logo"/></a>
+            <img className="header__logo_img" src={ImgLogo} alt="logo"/>
           </div>
           <div className="header__wrapper">
-            <ul className="header__menu">
-              <li className="header__menu_item">Главная</li>
-              <li className="header__menu_item">Автопарк</li>
-              <li className="header__menu_item">Аренда</li>
-              <li className="header__menu_item">Для бизнеса</li>
-              <li className="header__menu_item">Контакты</li>
-              <li className="header__menu_item">Личный кабинет</li>
-            </ul>
-            <div className="social">
-              <a className="social__link"><FontAwesomeIcon icon={faFacebook}/></a>
-              <a className="social__link"><FontAwesomeIcon icon={faInstagram}/></a>
-              <a className="social__link"><FontAwesomeIcon icon={faVk}/></a>
-              <a className="social__link"><FontAwesomeIcon icon={faWhatsapp}/></a>
+            <div className="header__wrapper_up">
+              <ul className="header__menu">
+                <li className="header__menu_item"><a href="#">Главная</a></li>
+                <li className="header__menu_item"><a href="#">Автопарк</a></li>
+                <li className="header__menu_item"><a href="#">Аренда</a></li>
+                <li className="header__menu_item"><a href="#">Для бизнеса</a></li>
+                <li className="header__menu_item"><a href="#">Контакты</a></li>
+                <li className="header__menu_item"><a href="#">Личный кабинет</a></li>
+              </ul>
+              <div className="header__social">
+                <a href="https://facebook.com" target="_blank" className="header__social_link"><FontAwesomeIcon icon={faFacebook}/></a>
+                <a href="https://instagram.com" target="_blank" className="header__social_link"><FontAwesomeIcon icon={faInstagram}/></a>
+                <a href="https://vk.com" target="_blank" className="header__social_link"><FontAwesomeIcon icon={faVk}/></a>
+                <a href="https://whatsapp.com" target="_blank" className="header__social_link"><FontAwesomeIcon icon={faWhatsapp}/></a>
+              </div>
             </div>
-            <div className="divider"></div>
-            <div className="header__phone">
-              <a href="tel:89958883094">8 (800) 555-35-35</a>
-            </div>
-            <div className="header__download">
-              <a href=""><img src="" alt="AppStore"/></a>
-              <a href=""><img src="" alt="GooglePlay"/></a>
+            <div className="header__divider"></div>
+            <div className="header__wrapper_down">
+              <div className="header__phone">
+                <a href="tel:89958883094">8 (800) 555-35-35</a>
+              </div>
+              <div className="header__download">
+                <a href=""><img src={AppStore} alt="AppStore"/></a>
+                <a href=""><img src={PlayMarket} alt="GooglePlay"/></a>
+              </div>
             </div>
           </div>
         </div>
